2d33771534e9e59abc3843e6b901ad0952984086
주경훈 <wnrudgns73@dxmcorp.com>
2021-02-26 18:52:21 +0900

    remove

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = function () {
-  return [
-    "77f88db7-8b13-4a17-8b8f-20a166e61fcc",
-    "2aa031b9a0565849f3a3ce8886205eae", //bithumb api key
-    "81e34c94c177c50a680238731a63f914"  //bithumb api secret
-
-  ]
-};
