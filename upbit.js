78528f6918f0d450269458c049cdd088fa7208da
주경훈 <wnrudgns73@dxmcorp.com>
2021-03-21 18:44:09 +0900

    upbit test

--- upbit.js
+++ upbit.js
@@ -71,25 +71,14 @@ UpbitAPI.prototype.minInfo = async function(params){
 }
 
 UpbitAPI.prototype.trade = async function(tradeType,market,price=null,volume=null){
-  var body;
-  if(tradeType =="bid"){
-    body = {
-      market: market,
-      side: tradeType,
-      price: price,
-      ord_type: 'price',
-    }
-  }else if(tradeType=="ask"){
-    body = {
+  const body = {
       market: market,
       side: tradeType,
       volume: volume,
       price: price,
       ord_type: 'price',
-    }
   }
-  console.log(body)
-
+console.log(body)
   return await this.request("/v1/orders",body,"POST");
 
 }
