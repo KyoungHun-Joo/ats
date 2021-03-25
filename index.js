a47fa00f637c792f186b82b80fd8a56411917c75
주경훈 <wnrudgns73@dxmcorp.com>
2021-03-25 10:21:19 +0900

    log add

--- index.js
+++ index.js
@@ -295,6 +295,8 @@ async function checkOrder(){
 
       if(data[i].type=='upbitMoney' || data[i].type=='upbit2Money'){
         var result = await upbit.orderInfo(data[i].order_id);
+        var coinPrice = await upbit.coinPrice(trade[0].slug);
+        console.log('now --',slug,coinPrice)
 
         if(result.state=="done"){
           const [leftValue, fileds] = await connection.execute("SELECT value FROM variable WHERE `key` = '"+data[i].type+"' ");
