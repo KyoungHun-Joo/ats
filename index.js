f745c7090f9bbbea78015bae61e745db950e7714
주경훈 <wnrudgns73@dxmcorp.com>
2021-09-10 14:38:36 +0900

    weight remove

--- index.js
+++ index.js
@@ -493,7 +493,7 @@ async function upbitTrade(connection) {
         console.log("market", market, lastRSI15, priceData[0].trade_price, weight, CONFIG.LOW_POINT);
         if ((await upbitCompare(1, rsiRes15, priceData[0].trade_price, 0, weight)) && !buyFlag) {
           buyFlag = true;
-          if(buyItem.rsi>rsiRes15-weight){
+          if(buyItem.rsi>rsiRes15){
             buyItem.market = market;
             buyItem.rsi = rsiRes15;
             buyItem.trade_price = priceData[0].trade_price
