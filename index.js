6e5c00124b378ce2fc0b868803f383564a8d083c
주경훈 <wnrudgns73@dxmcorp.com>
2021-09-10 15:41:39 +0900

    test

--- index.js
+++ index.js
@@ -493,6 +493,7 @@ async function upbitTrade(connection) {
         console.log("market", market, lastRSI15, priceData[0].trade_price, weight, CONFIG.LOW_POINT);
         if ((await upbitCompare(1, rsiRes15, priceData[0].trade_price, 0, weight))) {
           buyFlag = true;
+          console.log('true',buyItem.rsi, rsiRes15, buyItem.rsi>rsiRes15)
           if(buyItem.rsi>rsiRes15){
             console.log('buyitem2',market,rsiRes15, priceData[0].trade_price)
             buyItem.market = market;
